2f613862e59db5c7507a0350fc0e1bbf704d3b6e
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-01-19 20:58:42 -0300

    Criação funções comuns comuns.c

--- projeto_c/Source/comuns.c
+++ projeto_c/Source/comuns.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "../Headers/funcoes_comuns.h"
 
+int verificacao(char ident[]){
+    char aceitos[11] = "0123456789";
+    int cont = 0;
+    int multiplo, digito_verificador;
+    float soma_ponderada = 0;
+
+    if(strlen(ident) == 8){ //verifica se o identificador possui 8 digitos
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 10; j++){
+                if(ident[i] == aceitos[j]){ //verifica se todos os digitos são números e converte de string para integer
+                    cont++;
+                    codigo[i] = j;
+                }
+            }
+        }
+    }else{
+        printf("Identificador não possui 8 dígitos.\n");
+        return 1;
+    }
+    
+    if(cont != 8){
+        printf("Identificador contém valores não numéricos.\n");
+        return 1;
+    }
+
+    for(int i = 0; i < 7; i++){ //calcula a soma ponderada usada para calcular o digito verificador
+        if(i%2==0){
+            soma_ponderada += codigo[i]*3;
+        }else{
+            soma_ponderada += codigo[i];
+        }
+    }
+
+    multiplo = soma_ponderada;
+    if(multiplo%10 != 0){
+        multiplo = ceil(soma_ponderada/10)*10;
+    }
+
+    digito_verificador = multiplo - soma_ponderada; 
+    if(digito_verificador != codigo[7]){ //verifica se o digito verificador calculado é igual ao que aparece no identificador
+        printf("O dígito verificador do identificador é inválido (não corresponde aos valores anteriores).\n");
+        return 1;
+    }else{
+        return 0;
+    }
+}
