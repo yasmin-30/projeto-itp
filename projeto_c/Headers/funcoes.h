8c707c5a4a689bba2e39cb7aef51bcda9c992a18
TheoCamposSoares <161765940+TheoCamposSoares@users.noreply.github.com>
2025-01-19 20:40:32 -0300

    Add files via upload

---
+++ projeto_c/Headers/funcoes.h
@@ -0,0 +1,30 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+#include <stdio.h>
+
+typedef struct {
+    int largura;
+    int altura;
+    int **matriz;
+} ImagemPBM;
+
+extern const char *L_CODE[10];
+extern const char *R_CODE[10];
+
+ImagemPBM *carregar_imagem(const char *nome_arquivo);
+
+int validar_cabecalho(FILE *arquivo);
+
+int verificar_arquivo(const char *nome_arquivo);
+
+void liberar_imagem(ImagemPBM *imagem);
+
+int verificar_sequencia(int *linha, int inicio, const char *sequencia, int largura_modulo);
+
+int encontrar_codigo_barras(ImagemPBM *imagem);
+
+int decodificar_digito(const char *sequencia, int is_left);
+
+char *extrair_identificador(ImagemPBM *imagem);
+
+#endif // FUNCOES_H
