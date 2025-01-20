1dfec135750237c02a6d5ea110d45fc4fe37e769
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-01-19 21:09:15 -0300

    Update funcoes_gerador.h

--- projeto_c/Headers/funcoes_gerador.h
+++ projeto_c/Headers/funcoes_gerador.h
@@ -5,8 +5,6 @@ int codigo[8];
 char* left_code[11];
 char* right_code[11];
 
-
-int verificacao(char ident[]);
 char *conversor();
 int** preencher_codigo(char conversao[], int **imagem, int espacamento, int area, int altura);
 void gerar(int** imagem, int dimensao_x, int dimensao_y, char nome[]);
