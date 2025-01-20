a851b37641b7e614f1edb40f9195998e7264dfcd
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-01-19 21:01:44 -0300

    Update funcoes_gerador.c

--- projeto_c/Source/funcoes_gerador.c
+++ projeto_c/Source/funcoes_gerador.c
@@ -6,65 +6,15 @@
 
 int codigo[8]; //variável global por ser usada em muitas funções
 
-/*LEFT E RIGHT CODE - podem ser usados tanto para escanear (pega um conjunto de 7 digitos e compara com os do left ou right code para descobrir
-o número, que será o índice de um dos vetores) quanto para gerar o código*/
 char* left_code[] = {"0001101","0011001","0010011","0111101","0100011","0110001","0101111","0111011","0110111","0001011"};
 char* right_code[] = {"1110010","1100110","1101100","1000010","1011100","1001110","1010000","1000100","1001000","1110100"};
-
-//COLOCAR \n NOS TEXTOS
-int verificacao(char ident[]){
-    char aceitos[11] = "0123456789";
-    int cont = 0;
-    int multiplo, digito_verificador;
-    float soma_ponderada = 0;
-
-    if(strlen(ident) == 8){ //verifica se o identificador possui 8 digitos
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 10; j++){
-                if(ident[i] == aceitos[j]){ //verifica se todos os digitos são números e converte de string para integer
-                    cont++;
-                    codigo[i] = j;
-                }
-            }
-        }
-    }else{
-        printf("Identificador não possui 8 dígitos.\n");
-        return 1;
-    }
-    
-    if(cont != 8){
-        printf("Identificador contém valores não numéricos.\n");
-        return 1;
-    }
-
-    for(int i = 0; i < 7; i++){ //calcula a soma ponderada usada para calcular o digito verificador
-        if(i%2==0){
-            soma_ponderada += codigo[i]*3;
-        }else{
-            soma_ponderada += codigo[i];
-        }
-    }
-
-    multiplo = soma_ponderada;
-    if(multiplo%10 != 0){
-        multiplo = ceil(soma_ponderada/10)*10;
-    }
-
-    digito_verificador = multiplo - soma_ponderada; 
-    if(digito_verificador != codigo[7]){ //verifica se o digito verificador calculado é igual ao que aparece no identificador
-        printf("O dígito verificador do identificador é inválido (não corresponde aos valores anteriores).\n");
-        return 1;
-    }else{
-        return 0;
-    }
-}
 
 char* conversor(){  
     char *conversao;
     conversao = calloc(67, sizeof(char));
     strcpy(conversao, "101"); //marca inicial  
     
-    for(int i = 0; i < 8; i++){ //talvez colocar essa parte de formar a sequência em outra função
+    for(int i = 0; i < 8; i++){
         if(i < 4){
             strcat(conversao, left_code[codigo[i]]);
         }else{
