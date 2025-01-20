0747f211c7194584f91832832d97259c46c10173
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-01-19 21:04:59 -0300

    Update funcoes_comuns.c

--- projeto_c/Source/funcoes_comuns.c
+++ projeto_c/Source/funcoes_comuns.c
@@ -7,7 +7,7 @@
 char* left_code[] = {"0001101","0011001","0010011","0111101","0100011","0110001","0101111","0111011","0110111","0001011"};
 char* right_code[] = {"1110010","1100110","1101100","1000010","1011100","1001110","1010000","1000100","1001000","1110100"};
 
-int verificacao(char ident[]){
+int verificacao(char ident[], int codigo[]){
     char aceitos[11] = "0123456789";
     int cont = 0;
     int multiplo, digito_verificador;
