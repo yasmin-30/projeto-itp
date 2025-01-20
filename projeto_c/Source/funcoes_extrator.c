61b0f1d9aafaf929e19f8ad788fa0094d6110af5
TheoCamposSoares <161765940+TheoCamposSoares@users.noreply.github.com>
2025-01-19 21:05:57 -0300

    Update funcoes_extrator.c

--- projeto_c/Source/funcoes_extrator.c
+++ projeto_c/Source/funcoes_extrator.c
@@ -6,9 +6,6 @@
 #include "../Headers/funcoes_comuns.h"
 #define cabecalho_pbm "P1"
 
-const char *L_CODE[10] = {"0001101", "0011001", "0010011", "0111101", "0100011", "0110001", "0101111", "0111011", "0110111", "0001011"};
-const char *R_CODE[10] = {"1110010", "1100110", "1101100", "1000010", "1011100", "1001110", "1010000", "1000100", "1001000", "1110100"};
-
 
 ImagemPBM *carregar_imagem(const char *nome_arquivo) {
     FILE *arquivo = fopen(nome_arquivo, "r");
