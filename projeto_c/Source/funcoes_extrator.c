5c79695a39667a78cda3d445b09b09ec1cce8f8d
TheoCamposSoares <161765940+TheoCamposSoares@users.noreply.github.com>
2025-01-20 12:08:08 -0300

    Update funcoes_extrator.c

--- projeto_c/Source/funcoes_extrator.c
+++ projeto_c/Source/funcoes_extrator.c
@@ -110,24 +110,45 @@ int verificar_sequencia(int *linha, int inicio, const char *sequencia, int largu
     return 1;
 }
 
-int encontrar_codigo_barras(ImagemPBM *imagem) {
-    int meio = imagem->altura / 2; 
-    int largura_util = imagem->largura; 
-    int largura_modulo = largura_util / 67; 
+int encontrar_codigo_barras(const ImagemPBM* imagem) {
+    if (imagem->altura <= 0 || imagem->largura <= 0) {
+        return 0; // Imagem inválida
+    }
+
+    int meio = imagem->altura / 2; // Calcula a linha do meio
+    int largura_util = imagem->largura;
+    int largura_modulo = largura_util / 67;
 
     if (largura_modulo <= 0) {
+        return 0; // Largura do módulo inválida
+    }
+
+    // Aloca dinamicamente memória para a linha do meio
+    int* linha_meio = malloc(largura_util * sizeof(int));
+    if (!linha_meio) {
+        perror("Erro ao alocar memória para linha do meio");
         return 0;
     }
 
-    // largura do módulo
-    for (int j = 0; j <= largura_util - 67 * largura_modulo; j++) {
-        if (verificar_sequencia(imagem->matriz[meio], j, "101", largura_modulo) &&
-            verificar_sequencia(imagem->matriz[meio], j + 3 * largura_modulo + 28 * largura_modulo, "01010", largura_modulo) &&
-            verificar_sequencia(imagem->matriz[meio], j + 3 * largura_modulo + 28 * largura_modulo + 5 * largura_modulo + 28 * largura_modulo, "101", largura_modulo)) {
+    // Copia a linha do meio para a memória alocada
+    for (int j = 0; j < largura_util; j++) {
+        linha_meio[j] = imagem->matriz[meio][j];
+    }
+
+
+    // Verifica as sequências necessárias na linha do meio
+    int limite = largura_util - (3 * largura_modulo + 28 * largura_modulo + 5 * largura_modulo + 28 * largura_modulo);
+    for (int j = 0; j <= limite; j++) {
+        if (verificar_sequencia(linha_meio, j, "101", largura_modulo) &&
+            verificar_sequencia(linha_meio, j + 3 * largura_modulo + 28 * largura_modulo, "01010", largura_modulo) &&
+            verificar_sequencia(linha_meio, j + 3 * largura_modulo + 28 * largura_modulo + 5 * largura_modulo + 28 * largura_modulo, "101", largura_modulo)) {
+            free(linha_meio); // Libera a memória antes de retornar
             return largura_modulo;
         }
     }
-    return 0;
+
+    free(linha_meio); // Libera a memória alocada
+    return 0; // Código de barras não encontrado
 }
 
 int decodificar_digito(const char *sequencia, int is_left) {
